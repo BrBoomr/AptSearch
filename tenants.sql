7036c9f38db50b0f206e499ff2979d0510c04cfa
Jerry <jerry.guerrero95@gmail.com>
2018-09-11 17:38:23 -0500

    Added comments for clarification

--- tenants.sql
+++ tenants.sql
@@ -5,16 +5,18 @@ CREATE TABLE tenants (
     email VARCHAR(255),
     apartment_address VARCHAR(255),
     phone VARCHAR(255),
+    -- the ID of the landlord and apartment, which will be in another table
     landlord_id mediumint(8) unsigned,
     apartment_id mediumint(8) unsigned,
     -- DATE format: YYYY-MM-DD
     lease_start DATE,
     lease_end DATE,
     next_payment DATE,
+    --DECIMAL(max_size, precision)
     payment_amount DECIMAL(15, 2),
     PRIMARY KEY (tenant_id)
 ) AUTO_INCREMENT=1;
 
-
+-- Test insertion
 INSERT INTO tenants (first_name, last_name, email, apartment_address, phone, landlord_id, apartment_id, lease_start, lease_end, next_payment, payment_amount)
 VALUES ("Jerry", "Guerrero", "gerardo.guerrero02@utrgv.edu", "1234 Fake Avenue", "555-555-5555", 01234567, 12345678, "2018-09-11", "2018-12-11", "2018-10-11", 900.00) 
