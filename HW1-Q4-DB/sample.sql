681c415ab3c5a40bde922096b5357c9f6886b08f
BrBoomr <jrg7_br@hotmail.com>
2018-10-06 13:12:29 -0500

    Updated HW1-Q4-DB with underscore naming

--- HW1-Q4-DB/sample.sql
+++ HW1-Q4-DB/sample.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2018 at 07:09 PM
+-- Generation Time: Oct 06, 2018 at 08:11 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `accident` (
-  `report-number` int(11) NOT NULL,
+  `report_number` int(11) NOT NULL,
   `date` date NOT NULL,
   `location` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
@@ -38,14 +38,14 @@ CREATE TABLE `accident` (
 -- Dumping data for table `accident`
 --
 
-INSERT INTO `accident` (`report-number`, `date`, `location`) VALUES
+INSERT INTO `accident` (`report_number`, `date`, `location`) VALUES
 (1, '2018-10-06', 'Main Street'),
 (2, '2017-02-14', 'Valentine Street'),
 (3, '2017-04-20', 'Dank Street'),
-(4, '2017-12-31', 'Christ Street'),
-(5, '2015-03-17', 'Patrick Street'),
-(6, '2010-12-02', 'Hanukkah Street'),
-(7, '2012-01-01', 'Kony Street');
+(4, '1989-12-31', 'Christ Street'),
+(5, '1989-03-17', 'Patrick Street'),
+(6, '1989-12-02', 'Hanukkah Street'),
+(7, '1989-01-06', 'Kony Street');
 
 -- --------------------------------------------------------
 
@@ -54,7 +54,7 @@ INSERT INTO `accident` (`report-number`, `date`, `location`) VALUES
 --
 
 CREATE TABLE `car` (
-  `car-license` text NOT NULL,
+  `car_license` text NOT NULL,
   `model` text NOT NULL,
   `year` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
@@ -63,7 +63,7 @@ CREATE TABLE `car` (
 -- Dumping data for table `car`
 --
 
-INSERT INTO `car` (`car-license`, `model`, `year`) VALUES
+INSERT INTO `car` (`car_license`, `model`, `year`) VALUES
 ('H123-ABC', 'Hummer', 2006),
 ('M123-ABC', 'Mazda', 2008),
 ('L123-ABC', 'Lamborghini', 2012),
@@ -78,20 +78,20 @@ INSERT INTO `car` (`car-license`, `model`, `year`) VALUES
 --
 
 CREATE TABLE `owns` (
-  `driver-id` text NOT NULL,
-  `car-license` text NOT NULL
+  `driver_id` text NOT NULL,
+  `car_license` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
 
 --
 -- Dumping data for table `owns`
 --
 
-INSERT INTO `owns` (`driver-id`, `car-license`) VALUES
+INSERT INTO `owns` (`driver_id`, `car_license`) VALUES
 ('J123', 'H123-ABC'),
 ('B123', 'M123-ABC'),
 ('J321', 'L123-ABC'),
 ('A123', 'N123-ABC'),
-('B123', 'C123-ABC'),
+('B321', 'C123-ABC'),
 ('C123', 'F123-ABC');
 
 -- --------------------------------------------------------
@@ -101,23 +101,26 @@ INSERT INTO `owns` (`driver-id`, `car-license`) VALUES
 --
 
 CREATE TABLE `participated` (
-  `driver-id` text NOT NULL,
-  `car-license` text NOT NULL,
-  `report-number` int(11) NOT NULL,
-  `damage-amount` int(11) NOT NULL
+  `driver_id` text NOT NULL,
+  `car_license` text NOT NULL,
+  `report_number` int(11) NOT NULL,
+  `damage_amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
 
 --
 -- Dumping data for table `participated`
 --
 
-INSERT INTO `participated` (`driver-id`, `car-license`, `report-number`, `damage-amount`) VALUES
+INSERT INTO `participated` (`driver_id`, `car_license`, `report_number`, `damage_amount`) VALUES
 ('J123', 'H123-ABC', 1, 5000),
 ('A123', 'N123-ABC', 1, 5000),
 ('B123', 'M123-ABC', 2, 7500),
-('B123', 'C123-ABC', 2, 7500),
+('B321', 'C123-ABC', 2, 7500),
 ('J321', 'L123-ABC', 3, 10000),
-('C123', 'F123-ABC', 3, 10000);
+('C123', 'F123-ABC', 3, 10000),
+('J123', 'H123-ABC', 4, 6000),
+('B123', 'M123-ABC', 4, 6000),
+('J321', 'L123-ABC', 4, 6000);
 
 -- --------------------------------------------------------
 
@@ -126,7 +129,7 @@ INSERT INTO `participated` (`driver-id`, `car-license`, `report-number`, `damage
 --
 
 CREATE TABLE `person` (
-  `driver-id` text NOT NULL,
+  `driver_id` text NOT NULL,
   `name` text NOT NULL,
   `address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
@@ -135,12 +138,12 @@ CREATE TABLE `person` (
 -- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`driver-id`, `name`, `address`) VALUES
+INSERT INTO `person` (`driver_id`, `name`, `address`) VALUES
 ('J123', 'Jafet', '123 Main Street'),
 ('B123', 'Bryan', '321 Main Street'),
 ('J321', 'Jerry', '456 Main Street'),
 ('A123', 'Adam', '654 Main Street'),
-('B123', 'Ben', '789 Main Street'),
+('B321', 'Ben', '789 Main Street'),
 ('C123', 'Charlie', '987 Main Street');
 COMMIT;
 
