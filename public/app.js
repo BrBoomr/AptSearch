387f9bc46b96e2bc8f1164398c8b69189e489f6d
BrBoomr <jrg7_br@hotmail.com>
2018-11-25 20:38:17 -0600

    added features for registration, saving to email table needs fixing

--- public/app.js
+++ public/app.js
@@ -13,7 +13,7 @@ $('#loginSubmit').click(function(){
             if(response['verified'] == "true"){
                 $.ajax({
                     method: "post",
-                    url: baseurl + "/success",
+                    url: baseurl + "/success_login",
                     data: {"userID" : response['userID']},
                     dataType: "text",
                     success: function (r) {
@@ -27,6 +27,54 @@ $('#loginSubmit').click(function(){
         }
     });
 });
+
+$("#registerSubmit").click(function(){
+    event.preventDefault()
+    //registration = []
+    registerFirstName = $("#registerFirstName").val()
+    registerLastName = $("#registerLastName").val()
+    registerEmail = $("#registerEmail").val()
+    registerPassword = $("#registerPassword").val()
+    confirmPassword = $("#confirmPassword").val()
+    //registration.push(registerFirstName,registerLastName,registerEmail,registerPassword,confirmPassword)
+
+    $.ajax({
+        method: "get",
+        url: baseurl + "/register_verification",
+        data: {"firstName" : registerFirstName,
+                "lastName" : registerLastName,
+                "email" : registerEmail,
+                "password" : registerPassword,
+                "confirm" : confirmPassword},
+        dataType: "json",
+        success: function (response) {
+            if(response['verified']=='true'){
+                $.ajax({
+                    method: "get",
+                    url: baseurl + "/success_register",
+                    dataType: "text",
+                    success: function (r) {
+                        console.log("Successfuly Registered!")
+                        $("body").html(r)
+                    }
+                });
+            }
+            else if(response['mismatch'] == 'true'){
+                console.log("Passwords mismatch")
+            }
+            else if(response['invalid'] == 'true'){
+                console.log("Fields Cannot Be Empty!")
+            }
+            else if(response['duplicate']== 'true'){
+                console.log("Email already in use!")
+            }
+            else{
+                console.log("All's fucked")
+                //Internal Error Handling.
+            }
+        }
+    });
+})
 // When the user clicks the x on the error messages, hides them
 $(".close").click(function(){
     $(this).parent().hide()
--- public/app.php
+++ public/app.php
@@ -22,21 +22,16 @@ $container['view'] = function($container) {
 	
 	return $view;
 };
-$app->get('/createUser', function ($request, $response, $args) {
-	$user = new User();
-	$user->setFirstName("Zhixiang");
-	$user->setLastName("Chen");
-	$user->setPassword("zchen");
-	//$user->save();
-	echo ($user->getFirstName(). " created!");
-	return $response;
-});
 // home page route
 $app->get('/', function ($request, $response, $args) {
 	$this->view->render($response, "index.html");
 	return $response;
 });
 
+
+//////////////////////////////
+/////////LOGIN ROUTES/////////
+//////////////////////////////
 // The route for testing out the new login page
 $app->get('/login', function ($request, $response, $args) {
 	$this->view->render($response, "login.html");
@@ -51,7 +46,6 @@ $app->post('/login_verification', function ($request, $response, $args) {
 	//find user object in database
 	$email = EmailQuery::create()->findOneByEmail($email);
 	// If null is not caught, the following query will return a 500 error
-
 	if (is_null($email)){
 		$arr["verified"]="false";
 		return json_encode($arr);
@@ -69,9 +63,61 @@ $app->post('/login_verification', function ($request, $response, $args) {
 	return json_encode($arr);
 });
 
-$app->post("/success",function($request,$response,$args){
+$app->post("/success_login",function($request,$response,$args){
 	$userID = $this->request->getParam('userID');
 	$this->view->render($response, "index.html", ['user'=>UserQuery::create()->findPk($userID)]);
+	return $response;
+});
+
+//////////////////////////////
+///////REGISTER ROUTES////////
+//////////////////////////////
+function createUser($firstName, $lastName, $email, $password){
+	$user = new User();
+	$user->setFirstName($firstName);
+	$user->setLastName($lastName);
+	$user->setPassword($password);
+
+	$email = new Email();
+	$email->setEmail($email);
+	$email->setUserid($user->getId());
+	$email->setDescription("tenant");
+	$user->save();
+	$email->save();
+}
+$app->get('/register_verification', function ($request, $response, $args) {
+	$firstName = $this->request->getParam("firstName");
+	$lastName = $this->request->getParam("lastName");
+	$email = $this->request->getParam("email");
+	$password = $this->request->getParam("password");
+	$confirm = $this->request->getParam("confirm");
+
+	$fields = array($firstName, $lastName, $email , $password ,$confirm);
+	//CHECK IF ALL FIELDS ARE SET
+	foreach($fields as $field){
+		if(empty($field)){
+			//echo $field. " is empty!";
+			return json_encode( ['invalid' =>'true']);
+		}
+	}
+	//CHECK THAT PASSWORDS MATCH
+	if($password != $confirm){
+		//$code["invalid"]='true';
+		return json_encode( ["mismatch"=>'true'] );
+	}
+	//CHECK IF EMAIL IS ALREADY IN USE
+	$email = EmailQuery::create()->findOneByEmail($email);
+	if($email){
+		//$code["duplicate"]='true';
+		return json_encode(["duplicate"=>'true']);
+	}
+	createUser($firstName,$lastName,$email,$password);
+	return json_encode(["verified"=>"true"]);
+});
+
+$app->get('/success_register', function ($request, $response, $args) {
+	//echo "Success!</br>";
+	$this->view->render($response, "login.html");
 	return $response;
 });
 
