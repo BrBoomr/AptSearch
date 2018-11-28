259415b7bbe06655f585207c3e6653f74709a4ae
BrBoomr <jrg7_br@hotmail.com>
2018-11-27 20:52:03 -0600

    login working

--- public/app.php
+++ public/app.php
@@ -34,29 +34,37 @@ $app->get('/', function ($request, $response, $args) {
 //////////////////////////////
 // The route for testing out the new login page
 $app->get('/login', function ($request, $response, $args) {
+	
 	$this->view->render($response, "login.html");
 	return $response;
 });
 
 //login verification route
 $app->post('/login_verification', function ($request, $response, $args) {
-	$email = $this->request->getParams("email");
+	$email = $this->request->getParam("email");
 	$user = UserQuery::create()->findOneByEmail($email);
-	if($user && $user->login($this->request->getParams("password"))){
-		return ['verified' => 'true', 'userID' => $user->getId()];
+
+	if($user && $user->login($this->request->getParam("password"))){
+		return json_encode(['verified' => 'true', 'userID' => $user->getId()]);
 	}
-	return ['verified' => 'false'];
+	return json_encode(['verified' => 'false']);
 });
 
 $app->post("/success_login",function($request,$response,$args){
+	$userID = $this->request->getParam('userID');
+	$this->view->render($response, "index.html", ['user'=>UserQuery::create()->findPk($userID)]);
 	return $response;
 });
 
 //////////////////////////////
 ///////REGISTER ROUTES////////
 //////////////////////////////
-function createUser($firstName, $lastName, $email, $type, $password){
-	
+function createUser($name, $email, $password){
+	$newUser = new User();
+	$newUser->setName($name);
+	$newUser->setEmail($email);
+	$newUser->setPassword($password);
+	$newUser->save();
 }
 $app->get('/register_verification', function ($request, $response, $args) {
 
