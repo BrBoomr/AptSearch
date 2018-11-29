cb73de1da9c4aebc79fba7185b5aa810e4fdfddb
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-11-29 17:07:55 -0600

    another carousel update... next to fix the  title and subtitle text areas... then to once again try to have the image fit its box...

--- TEMPLATES/search/javascript/logic.js
+++ TEMPLATES/search/javascript/logic.js
@@ -1,60 +1,38 @@
-imagesInSlides = $(".imageInSlide")
-for (i = 0; i < imagesInSlides.length; i++) { 
-    //console.log(i + ": " + $(imagesInSlides).eq(i).attr('src'))
-    
-    $("<img/>") //make a new image tag
-    .attr("src", $(imagesInSlides).eq(i).attr("src")) //only copy over the source of our value
-    .on('load', function() {
-        //grab size of image from memory
-        width = this.width;  
-        height = this.height; 
-
-        //set the size of each image by initially stretching
-        $(imagesInSlides).eq(i).css('height', '100%')
-        $(imagesInSlides).eq(i).css('width', '100%')
-
-        //correct the image stretch if needed
-        if(width != height){
-            if(height > width){
-                console.log("height larger")
-               $(imagesInSlides).eq(i).css('width', 'auto')
-            }
-            else{
-                console.log("width larger")
-               $(imagesInSlides).eq(i).css('height', 'auto')
-            }
-        }
-    })
-}
-
-
+/*stops the carousel from automatically running*/
+$('.carousel').carousel({
+    interval: false
+}); 
 
 /*
 $( document ).ready(function() {
-    imagesSource = $(".imageSource")
-imagesInCarousel = $(".imageInCarousel")
-imagesInThumbnail = $(".imagesInThumbnail")
-$.each(imagesSource, function( index, value ) {
-    console.log("hey")
-    console.log($(value).naturalWidth)
-    console.log($(value).naturalHeight)
-    
-    height = $(images[index]).outerHeight()
-    width = $(images[index]).outerWidth()
+    //grab all of our images that are in the slides
+    imagesInSlides = $(".imageInSlide")
 
-    $(images[index]).css('height', '100%')
-    $(images[index]).css('width', '100%')
-    if(height != width){
-        if(height > width){
-            console.log("height larger")
-            $(images[index]).css('width', 'auto')
-        }
-        else{
-            console.log("width larger")
-            $(images[index]).css('height', 'auto')
-        }
+    //iterate through the list so the slide container can contain the image properly
+    for (i = 0; i < imagesInSlides.length; i++) { 
+        $("<img/>") //make a new image tag
+        .attr("src", $(imagesInSlides).eq(i).attr("src")) //only copy over the source of our value
+        .on('load', function() {
+            //grab size of image from memory
+            width = this.width;  
+            height = this.height; 
+
+            //set the size of each image by initially stretching
+            $(imagesInSlides).eq(i).css('height', '100%')
+            $(imagesInSlides).eq(i).css('width', '100%')
+
+            //correct the image stretch if needed
+            if(width != height){
+                if(height > width){
+                    console.log("height larger")
+                $(imagesInSlides).eq(i).css('width', 'auto')
+                }
+                else{
+                    console.log("width larger")
+                $(imagesInSlides).eq(i).css('height', 'auto')
+                }
+            }
+        })
     }
-    
-});
 });
 */
