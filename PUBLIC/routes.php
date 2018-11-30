2762e45f075c7e1f31b719ac5035b827e22fa11f
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-11-30 06:16:30 -0600

    all routes except the idk looks important section have been properly setup and include instructions on what each requires

--- PUBLIC/routes.php
+++ PUBLIC/routes.php
@@ -20,6 +20,11 @@ $app->get('/', function ($request, $response, $args) {
 	return $response;
 });
 
+$app->get('/viewProperty', function ($request, $response, $args) {
+	$this->view->render($response, "/viewProperty/html.html");
+	return $response;
+});
+
 //--------------------------------------------------MUST NOT BE AUTHENTICATED--------------------------------------------------
 
 //-------------------------AUTHENTICATION PAGE-------------------------
@@ -180,57 +185,7 @@ $app->get('/TEMPLATE', function ($request, $response, $args) {
 	return $response;
 });
 
-//--------------------------------------------------ONE BIG MESS--------------------------------------------------
-
-$app->get('/view_all_listing', function ($request, $response, $args) {
-	$listings = PropertyQuery::create();
-	$this->view->render($response, "listing/html.html",['user'=>current_user(),'listings'=>$listings]);
-	return $response;
-});
-
-$app->get('/view_my_listing', function ($request, $response, $args) {
-	if(current_user()){
-		$current_user = $_SESSION['user'];
-		$listings = PropertyQuery::create()->filterByUserid($current_user->getId());
-		$this->view->render($response, "listing/html.html",['user'=>current_user(), 'listings'=>$listings,]);
-	}
-	else{
-		$this->view->render($response, "authentication/html.html");
-	}
-	
-	return $response;
-});
-
-$app->get('/add_listing', function ($request, $response, $args) {
-	//createAddress(1,321,"Texas","Mission",78572,"East Street",1,1);
-	//createPropery(37,5,"W.B1.A1",TRUE, 900, 750 ,1,1,"Single Bedroom");
-
-	return $response;
-});
-
-// Verifies that the credentials are valid
-$app->post('/login_verification', function ($request, $response, $args) {
-	$email = $this->request->getParam("email");
-	$user = UserQuery::create()->findOneByEmail($email);
-
-	if($user && $user->login($this->request->getParam("password"))){
-		return json_encode(['verified' => 'true', 'userID' => $user->getId()]);
-	}
-	return json_encode(['verified' => 'false']);
-});
-
-$app->post('/update_listing', function ($request, $response, $args) {
-	
-});
-
-$app->get('/add_property', function ($request, $response, $args) {
-	if(current_user()){
-		$this->view->render($response, "/addProperty/html.html", ['user'=>current_user()]);
-		return $response;
-	}
-	$this->view->render($response, "/authentication/html.html", ['user'=>current_user()]);
-	return $response;
-});
+//--------------------------------------------------IDK LOOKS IMPORTANT--------------------------------------------------
 
 $app->post('/verify_property', function ($request, $response, $args) {
 	//$fields = $this->request->getQueryParams();
