e24ebb0c989d721b1b7ae78870a9d275654fd025
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-12-05 20:17:33 -0600

    sliders are nearly working... its like the 5th time ive said that...

--- TEMPLATES/properties/js.js
+++ TEMPLATES/properties/js.js
@@ -1,79 +1,122 @@
 //-------------------------Connections-------------------------
 
-var rentSlider = "#rentSection > div > .theSlider"
-var sqrft = "#sqrftSection > div > .theSlider"
-var bedSlider = "#bedSection > div > .theSlider"
-var bathSlider = "#bathSection > div > .theSlider"
-
 //-------------------------Logic-------------------------
 
+//-------------------------Slide Connection
 
+//wait for our document to load so that we can read the min and max of our sliders from the dom
 $( document ).ready(function() {
 
-    $(rentSlider).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 0, 500 ],
-        slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        },
-        stop: function(event, ui){
-            console.log("slide finshed at " + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
+    sliderContainers = $(".sliderContainer")
+    $(sliderContainers).each(function(index){
 
-    $(sqrft).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 0, 500 ],
-        slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        },
-        stop: function(event, ui){
-            console.log("slide finshed at " + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
+        //conntect to all the require dom elements
+        sliderContainer = $(sliderContainers).eq(index)
+        minText = $(sliderContainer).find("div > div > .minText")
+        maxText = $(sliderContainer).find("div > div > .maxText")
+        slider = $(sliderContainer).find("div > .theSlider")
 
-    $(bedSlider).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 0, 500 ],
-        slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        },
-        stop: function(event, ui){
-            console.log("slide finshed at " + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
+        //get our actual min/max from placeholder attributes
+        actualMin = parseInt(minText.attr('placeholder'))
+        actualMax = parseInt(maxText.attr('placeholder'))
 
-    $(bathSlider).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 0, 500 ],
-        slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        },
-        stop: function(event, ui){
-            console.log("slide finshed at " + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
+        //get our user min/max from our value attributes 
+        userMin = parseInt(minText.val())
+        userMax = parseInt(maxText.val())
+        //since we might not have a user min/max make sure that we have some value to take its place
+        userMin = (userMin) ? userMin : actualMin
+        userMax = (userMax) ? userMax : actualMax
 
+        //create a slider with the proper min, max, start min, and start max
+        $(slider).slider({
+            range: true,
+            max: actualMax,
+            min: actualMin,
+            values: [ userMin , userMax ],
+            //when either slider point is moved update the text boxes [UPDATE]
+            slide: function( event, ui ) { 
+                minTextBox = $(this).parent().parent().find("div > div > .minText")
+                maxTextBox = $(this).parent().parent().find("div > div > .maxText")
+                if(ui.handleIndex == 0) minTextBox.val(ui.values[0])
+                else maxTextBox.val(ui.values[1])
+            },
+            //output the new values when you let go of the slider [OUTPUT]
+            stop: function(event, ui){
+                if(ui.handleIndex == 0) console.log("-----NEW min: " + ui.values[ 0 ]);
+                else console.log("-----NEW max: " + ui.values[1]);
+            }
+        });
+        
+        function getInRangeVersionOfTexBoxValue(textBox, isMin){
+            newVal = $(textBox).val()
+            theSlider = $(textBox).parent().parent().parent().find("div > .theSlider")
+            if(newVal){ //make sure the new value exists
+                //create the range variables for each of the sliders
+                low = (isMin) ? $(theSlider).slider("option", "min") : $(theSlider).slider("values", 0)
+                high = (isMin) ? $(theSlider).slider("values", 1) : high = $(theSlider).slider("option", "max")
+                
+                //if we are not within range make ourselves within range
+                if(newVal < low && high < newVal){
+                    if(newVal < low) newVal = low
+                    else newVal = high
+                }
+            } //set the slider value to its default
+            else newVal = $(theSlider).slider("option", (isMin) ? "min" : "max")
+
+            //set the new value of the slider
+            $(theSlider).slider("values", (isMin) ? 0 : 1, newVal)
+
+            //return the newValue
+            return newVal
+        }
+
+        //when the text changes update the slider [UPDATE]
+        //DO NOT MESS with what the user is typing
+        function textBoxToSlider(textBox, isMin){
+            $(textBox).on("change keyup paste", function(){
+                getInRangeVersionOfTexBoxValue(this, isMin)
+            })
+        }
+
+        textBoxToSlider(minText, true)
+        textBoxToSlider(maxText, false)
+
+        function textBoxSubmitOnUnFocus(textBox, isMin) {
+            $(textBox).focusout(function() {
+                newVal = getInRangeVersionOfTexBoxValue(this, isMin)
+                //if they instead erased the value dont fill in the value
+                //allow the placeholder to be shown instead
+                if($(this).val()) $(this).val(newVal) 
+                console.log("-----NEW " + (isMin) ? "min" : "max" + " " + newVal)
+            })
+        }
+
+        textBoxSubmitOnUnFocus(minText, true)
+        textBoxSubmitOnUnFocus(maxText, false)
+
+        function textBoxSubmitOnEnter(textBox, isMin) {
+            $(textBox).keypress(function(key) {
+                if (event.which == 13 ) { //Enter key
+                    $(this).blur() //triggers text submission through unfocusing (above)
+                }
+            });
+        }
+
+        textBoxSubmitOnEnter(minText, true)
+        textBoxSubmitOnEnter(maxText, false)
+    })
 });
 
 
 //FOR DEBUGGING
-/*
+
 var openButtons = $(".sectionOpenButton")
 $.each(openButtons, function(index, openButton){
-    $("#" + $(openButton).attr("openRowID")).show()
+    $("#" + $(openButton).attr("openSectionID")).show()
 })
-*/
 
-//-------------------------Open Buttons
+
+//-------------------------Open/Close Buttons
 
 $(".sectionOpenButton").click(function(){
     //grab the name of the section we want to OPEN
@@ -104,9 +147,5 @@ $(".sectionOpenButton").click(function(){
         })
     }
 })
-
-//-------------------------Close Buttons
-
-
 
 //-------------------------Objects-------------------------
